90d63e38d832b4abe0ce0f2d48cf9c80200d34f9
RahulRaushan1 <rahulraushan137138@gmail.com>
2026-02-13 01:11:57 +0530

    Custom Exception

---
+++ Exception_Handling/CustomException/A1_InsufficientBalException.java
@@ -0,0 +1,11 @@
+package Exception_Handling.CustomException;
+
+public class A1_InsufficientBalException extends Exception {
+
+    public A1_InsufficientBalException(String msg)
+
+    {
+        super(msg);
+    }
+
+}
