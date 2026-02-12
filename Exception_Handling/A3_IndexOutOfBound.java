e2cedd35a4a25763cf7fd8190dd7c4dd3aeeaf1b
RahulRaushan1 <rahulraushan137138@gmail.com>
2026-02-13 01:01:15 +0530

    Runtime Exception

---
+++ Exception_Handling/A3_IndexOutOfBound.java
@@ -0,0 +1,8 @@
+package Exception_Handling;
+
+public class A3_IndexOutOfBound {
+    public static void main(String[] args) {
+        int[] arr = {10,11};
+        System.out.println(arr[4]);
+    }
+}
