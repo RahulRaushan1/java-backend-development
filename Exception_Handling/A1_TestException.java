1da0bf1bf0c092686add25ed917e469652acd067
RahulRaushan1 <rahulraushan137138@gmail.com>
2026-02-13 00:01:16 +0530

    Test Exception

---
+++ Exception_Handling/A1_TestException.java
@@ -0,0 +1,9 @@
+package Exception_Handling;
+
+public class A1_TestException {
+    public static void main(String[] args) {
+
+        System.out.println(10/0);
+
+    }
+}
---
+++ String/A1_StringDemo.java
@@ -0,0 +1,13 @@
+package String;
+
+public class A1_StringDemo {
+    public static void main(String[] args) {
+        String s1 = new String("Ashwani");
+        String s2 = "Ashwani";
+        String s3 = "Ayush";
+        System.out.println(s1.hashCode());
+        System.out.println(s2.hashCode());
+        System.out.println(s3.hashCode());
+
+    }
+}
