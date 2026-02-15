ef01e4faf21b76b7238dfbbd97d659402801e7b2
RahulRaushan1 <rahulraushan137138@gmail.com>
2026-02-16 00:21:23 +0530

    Abstract Demo

---
+++ AbstractDemo/A2_Thar.java
@@ -0,0 +1,20 @@
+package AbstractDemo;
+
+public class A2_Thar extends A1_Vehicle{
+
+    A2_Thar(String brand) {
+        super(brand);   // parent class constructor
+
+    }
+
+    @Override
+    void start() {
+
+    }
+
+    public static void main (String[] args){
+      //  A1_Vehicle c = new A1_Vehicle() ;
+
+
+    }
+}
