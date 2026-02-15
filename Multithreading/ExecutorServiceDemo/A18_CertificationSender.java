b9dccdaf07f18e10cd9b1f0865d0a0b7c87d3f30
RahulRaushan1 <rahulraushan137138@gmail.com>
2026-02-16 00:18:07 +0530

    Executor Service

---
+++ Multithreading/ExecutorServiceDemo/A18_CertificationSender.java
@@ -0,0 +1,20 @@
+package Multithreading.ExecutorServiceDemo;
+
+public class A18_CertificationSender implements Runnable{
+    private final String stdName;
+
+    public A18_CertificationSender(String stdName){
+        this.stdName=stdName;
+    }
+    @Override
+    public void run() {
+        System.out.println("Sending certificate to "+stdName+
+                "by thread" + Thread.currentThread().getName());
+        try {
+            Thread.sleep(2000);
+        }catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+        //System.out.println("certificate sent to \"+stdName + \n" + );
+    }
+}
