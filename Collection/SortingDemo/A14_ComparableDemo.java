4493362b2b2f541387f0156d182d87236056ae54
RahulRaushan1 <rahulraushan137138@gmail.com>
2026-02-19 23:34:17 +0530

    Comparable Demo

---
+++ Collection/SortingDemo/A14_ComparableDemo.java
@@ -0,0 +1,22 @@
+package Collection.SortingDemo;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class A14_ComparableDemo {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(99);
+        al.add(1);
+        al.add(2);
+        al.add(0);
+        al.add(5);
+        al.add(101);
+
+        System.out.println(al);
+        Collections.sort(al);
+        System.out.println(al);
+
+    }
+}
