81705f38f8e1a5d372286b5dcf985c732a6e87da
RahulRaushan1 <rahulraushan137138@gmail.com>
2026-02-23 23:40:01 +0530

    Sorting

---
+++ Collection/Sorting/A21_Student.java
@@ -0,0 +1,46 @@
+package Collection.Sorting;
+
+public class A21_Student {
+    int roll;
+    String name;
+    int marks;
+
+    public A21_Student(int roll, String name, int marks) {
+        this.roll = roll;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+        return "A21_Student{" +
+                "roll=" + roll +
+                ", name='" + name + '\'' +
+                ", marks=" + marks +
+                '}';
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+}
