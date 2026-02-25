17db23d63b8a299f3092b6fdca199231412f125f
RahulRaushan1 <rahulraushan137138@gmail.com>
2026-02-25 13:14:55 +0530

    Delete

---
+++ JDBCP01/A4_JDBC_Delete.java
@@ -0,0 +1,31 @@
+package JDBCP01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class A4_JDBC_Delete {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // load driver class
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://127.0.0.1:3306/mystd?useSSL=false&allowPublicKeyRetrieval=true",
+                    "root",
+                    "Mysql@12"
+            );
+            Statement statement=con.createStatement();
+            //String query="select * from students";
+            String query = "DELETE From students where id=1";
+
+            // ResultSet rs=statement.executeQuery(query);
+            int update = statement.executeUpdate(query);
+            System.out.println("Updated"+update+"rows");
+
+
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
