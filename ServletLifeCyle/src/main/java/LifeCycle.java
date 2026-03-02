4a026655446e980488d8d096a2560483cbbbcea4
RahulRaushan1 <rahulraushan137138@gmail.com>
2026-03-02 10:04:10 +0530

    ServletLifeCycle

--- ServletLifeCyle/src/main/java/LifeCycle.java
+++ ServletLifeCyle/src/main/java/LifeCycle.java
@@ -4,6 +4,7 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class LifeCycle extends GenericServlet {
 
@@ -12,8 +13,14 @@ public class LifeCycle extends GenericServlet {
     }
 
     @Override
-    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
         System.out.println("service () called  - Handling Request");
+        res.setContentType("text/html");
+        PrintWriter out=res.getWriter();
+        out.println("<h2> Hello from LifeCycle ! </h2>");
 
+    }
+    public void destroy(){
+        System.out.println("destroy() called - servlet is destroyed.... ");
     }
 }
