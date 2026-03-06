5f1517b11b0f0bb0e00d10f62f903b86c1e1f804
RahulRaushan1 <rahulraushan137138@gmail.com>
2026-03-06 23:22:48 +0530

    TightCoupling

---
+++ TightCoupling/src/EngineFactory.java
@@ -0,0 +1,14 @@
+public class EngineFactory {
+
+    public static Engine getEngine(String type){
+        if(type.equalsIgnoreCase("diesel")){
+            return new DieselEngine();
+        }
+        else if (type.equalsIgnoreCase("petrol")){
+            return new PetrolEngine();
+        }
+        else{
+            throw new IllegalArgumentException("Invalid engine type");
+        }
+    }
+}
