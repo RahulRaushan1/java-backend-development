17ec46959244d574b99e43fe6eedcee6275a568c
RahulRaushan1 <rahulraushan137138@gmail.com>
2026-03-06 23:22:36 +0530

    TightCoupling

---
+++ TightCoupling/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+
+
+    public static void main(String[] args) {
+        Engine engine= EngineFactory.getEngine("petrol");
+        Car car= new Car(engine);
+        car.drive();
+    }
+}
