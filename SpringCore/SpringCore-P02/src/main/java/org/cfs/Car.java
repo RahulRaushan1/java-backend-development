0e47ef6f0d9b1a6efec472a1700ab95a010ff4cf
RahulRaushan1 <rahulraushan137138@gmail.com>
2026-03-08 20:12:57 +0530

    ModesOfAutowiring

---
+++ SpringCore/SpringCore-P02/src/main/java/org/cfs/Car.java
@@ -0,0 +1,34 @@
+package org.cfs;
+
+public class Car {
+
+    private Engine engine ;
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public Car(){
+        System.out.println("car default constructor....");
+    };
+
+    public void setEngine(Engine engine) {
+        System.out.println("setEngine() called...");
+        this.engine = engine;
+    }
+
+    public Car(Engine engine) {
+        System.out.println(" Car constructor running....");
+        this.engine = engine;
+    }
+
+    public void drive(){
+        int start = engine.start();
+        if(start>=1){
+            System.out.println("Let's drive");
+        }
+        else{
+            System.out.println("Engine not started yet....");
+        }
+    }
+}
