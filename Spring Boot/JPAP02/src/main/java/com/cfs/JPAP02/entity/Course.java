93771f7ea2625bf7dd654b25d106f51c6e630722
RahulRaushan1 <rahulraushan137138@gmail.com>
2026-03-12 14:11:50 +0530

    JPAP02_Mapping

---
+++ Spring Boot/JPAP02/src/main/java/com/cfs/JPAP02/entity/Course.java
@@ -0,0 +1,22 @@
+package com.cfs.JPAP02.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String title;
+
+    @ManyToMany(mappedBy = "course")
+    @JsonIgnoreProperties("course")
+    private Set<Student> student=new HashSet<>();
+}
