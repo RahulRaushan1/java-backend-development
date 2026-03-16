a269d3de8f795ba5d3938606907c22568ea219fb
RahulRaushan1 <rahulraushan137138@gmail.com>
2026-03-16 11:31:37 +0530

    Security_DemoP01

---
+++ SpringSecurity/Security-DemoP01/src/main/java/com/cfs/Security_DemoP01/controller/BankController.java
@@ -0,0 +1,27 @@
+package com.cfs.Security_DemoP01.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BankController {
+
+    int bal=1000;
+
+    @GetMapping("/bal")
+    public int getBalance(){
+        return bal;
+    }
+
+    @PostMapping("/add")
+    public int updateBalance(@RequestParam String AccNo,@RequestParam int num){
+        return bal+num;
+    }
+
+    @GetMapping("/contact")
+    private String contactUS(){
+        return "you can contact us";
+    }
+}
