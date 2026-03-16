d34b5fabf9e4d8fd6775d8f411b1443a931ed632
RahulRaushan1 <rahulraushan137138@gmail.com>
2026-03-16 20:38:00 +0530

    Security_DemoP02

---
+++ SpringSecurity/Security_DemoP02/src/main/java/com/cfs/Security_DemoP02/SecurityDemoP02Application.java
@@ -0,0 +1,13 @@
+package com.cfs.Security_DemoP02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDemoP02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityDemoP02Application.class, args);
+	}
+
+}
