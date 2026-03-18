83941906ea8b657f5cc0d519924e2dc5c7cd5348
RahulRaushan1 <rahulraushan137138@gmail.com>
2026-03-19 02:00:09 +0530

    SpringSecurityProject

---
+++ SpringSecurity/Security_DemoP04/src/main/java/com/cfs/Spring_DemoP04/SpringDemoP04Application.java
@@ -0,0 +1,13 @@
+package com.cfs.Spring_DemoP04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoP04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoP04Application.class, args);
+	}
+
+}
